269448a71f432438fcdaf2e26d23271b47e6ac87
Surupa Chatterjee <chatterjee.surupa63@gmail.com>
2020-10-20 17:02:02 -0500

    correct server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/newtonflix-client'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/newtonflix-client'));
+res.sendFile(path.join(__dirname+'/dist/newtonflix-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
